5455c7e32db57676a0e57e5925534affba6ffa12
shaalx <1316011195@qq.com>
2015-11-15 15:28:42 +0800

    md

---
+++ md/README.md
@@ -0,0 +1,41 @@
+#	Markdown
+
+
+This is a _test_
+
+This is a *test*
+
+This is a **test**
+
+This is a ==test==
+
+This is a --test--
+
+
+Name        | Age
+------------|------
+Bob     	| 27
+Alice   	| 23
+
+
+``` go
+func getTrue() bool {
+    return true
+}
+```
+
+Cat
+: Fluffy animal everyone likes
+
+Internet
+: Vector of transmission for pictures of cats
+
+This is a footnote.[^1]
+
+[^1]: the footnote text.
+
+http://upload.daoapp.io/
+
+~~cross out~~
+
+4/5
--- md/main.go
+++ md/main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/microcosm-cc/bluemonday"
-	"github.com/russross/blackfriday"
+	// "github.com/microcosm-cc/bluemonday"
+	// "github.com/russross/blackfriday"
 	"github.com/shaalx/goutils"
+	md "github.com/shurcooL/github_flavored_markdown"
 	"io/ioutil"
 	"os"
 	"text/template"
@@ -24,10 +25,10 @@ const (
 <head>
 	<meta charset="utf-8">
 	<title>{{.Title}}</title>
-	<!-- <link href="http://cdn.bootcss.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+	<link href="http://cdn.bootcss.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
     <link href="http://cdn.bootcss.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href="http://static.bootcss.com/www/assets/css/site.min.css?v5" rel="stylesheet"> -->
-    <link crossorigin="anonymous" href="https://assets-cdn.github.com/assets/github-6670887f84dea33391b25bf5af0455816ab82a9bec8f4f5e4d8160d53b08c0f3.css" integrity="sha256-ZnCIf4TeozORslv1rwRVgWq4Kpvsj09eTYFg1TsIwPM=" media="all" rel="stylesheet" />
+    <link href="http://static.bootcss.com/www/assets/css/site.min.css?v5" rel="stylesheet">
+    <!-- <link crossorigin="anonymous" href="https://assets-cdn.github.com/assets/github-6670887f84dea33391b25bf5af0455816ab82a9bec8f4f5e4d8160d53b08c0f3.css" integrity="sha256-ZnCIf4TeozORslv1rwRVgWq4Kpvsj09eTYFg1TsIwPM=" media="all" rel="stylesheet" /> -->
     <link crossorigin="anonymous" href="https://assets-cdn.github.com/assets/github2-53964e9b93636aa437196c028e3b15febd3c6d5a52d4e8368a9c2894932d294e.css" integrity="sha256-U5ZOm5NjaqQ3GWwCjjsV/r08bVpS1Og2ipwolJMtKU4=" media="all" rel="stylesheet" />
     {{.MoreCss}}
 </head>
@@ -72,8 +73,9 @@ func renderFile(filename string, redo bool) bool {
 	if nil == input {
 		return false
 	}
-	unsafe := blackfriday.MarkdownCommon(input)
-	html := bluemonday.UGCPolicy().SanitizeBytes(unsafe)
+	// unsafe := blackfriday.MarkdownCommon(input)
+	// html := bluemonday.UGCPolicy().SanitizeBytes(unsafe)
+	html := md.Markdown(input)
 
 	data := make(map[string]interface{})
 	data["MDContent"] = goutils.ToString(html)
