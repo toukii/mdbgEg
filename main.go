2d7e309ea890f39488bf99139f6e999632e4de45
shaalx <1316011195@qq.com>
2015-11-21 12:44:00 +0800

    auto

--- main.go
+++ main.go
@@ -1,65 +1,20 @@
 package main
 
 import (
-	"github.com/microcosm-cc/bluemonday"
-	"github.com/russross/blackfriday"
-	"github.com/shaalx/goutils"
-	"io/ioutil"
-	"os"
-	"text/template"
+	"github.com/everfore/exc"
+	// "github.com/shaalx/goutils"
 )
 
 var (
-	theme *template.Template
-)
-
-const (
-	thm_file = "theme.thm"
+	exc_cmd *exc.CMD
 )
 
 func init() {
-	thm_b := readFile(thm_file)
-	var err error
-	theme, err = template.New("theme.thm").Parse(goutils.ToString(thm_b))
-	if goutils.CheckErr(err) {
-		panic(err.Error())
-	}
+	exc_cmd = exc.NewCMD("rdr").Debug()
+	exc_cmd.Execute()
+	exc_cmd.Reset("ext -f").Execute()
 }
 
 func main() {
-	filename := "README.md"
-	renderFile(filename)
-}
 
-func renderFile(filename string) bool {
-	input := readFile(filename)
-	unsafe := blackfriday.MarkdownCommon(input)
-	html := bluemonday.UGCPolicy().SanitizeBytes(unsafe)
-
-	data := make(map[string]interface{})
-	data["MDContent"] = goutils.ToString(html)
-	data["Title"] = filename
-	of, err := os.OpenFile(filename+".html", os.O_CREATE|os.O_RDWR, 0666)
-	defer of.Close()
-	if goutils.CheckErr(err) {
-		return false
-	}
-	err = theme.Execute(of, data)
-	if goutils.CheckErr(err) {
-		return false
-	}
-	return true
-}
-
-func readFile(filename string) []byte {
-	file, err := os.OpenFile(filename, os.O_RDONLY, 0644)
-	if goutils.CheckErr(err) {
-		return nil
-	}
-	defer file.Close()
-	b, err := ioutil.ReadAll(file)
-	if goutils.CheckErr(err) {
-		return nil
-	}
-	return b
 }
