0b99703f8cb3f11e077340fbf62f7776c8365b39
shaalx <shaalx@yahoo.com.sg>
2015-11-27 21:26:18 +0800

    auto

--- md/main.go
+++ md/main.go
@@ -43,6 +43,7 @@ const (
 					<ul class="nav navbar-nav" id="menu">
 						<li><a href="/">Home</a></li>
 						<li><a href="/Go">Go</a></li>
+						<li><a href="/Linux">Linux</a></li>
 						<li><a href="/Item">Item</a></li>
 					</ul>
 				</div>
